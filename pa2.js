19b087420fc517b466bd0a5288f40b30dc724722
iamcute <2570394541@qq.com>
2017-09-29 20:34:47 +0800

    superagent+cheerio+more page

--- pa2.js
+++ pa2.js
@@ -8,26 +8,52 @@ var cheerio = require('cheerio');
 // var express = require('express');
 // var app = express();
 
-superagent.get('https://movie.douban.com/review/best/?start=0')
-    .end(function (err, res) {
-        if (err) {
-            return console.error(err);
-        }
-        var comment = [];
-        var infors = '';
-        var $ = cheerio.load(res.text);
-        var all = $('.header-more');
-        all.each(function (i,item) {
-            item = $(this);
-            var items = item.find('a').text().split('\n');
-            comment.push({
-                author:items[1].trim(),
-                title:items[2].trim()
+var page = 0;
+var url = 'https://movie.douban.com/review/best/?start=';
+function getComments(url, page) {
+    superagent.get(url + page)
+        .end(function (err, res) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('正在获取第'+page+'页热门评论');
+
+
+            var comment = [];
+            var infors = '';
+            var $ = cheerio.load(res.text);
+            var all = $('.header-more');
+
+            all.each(function (i, item) {
+                item = $(this);
+                var items = item.find('a').text().split('\n');
+                comment.push({
+                    author: items[1].trim(),
+                    title: items[2].trim()
+                });
             });
+            comment.map(function (infor) {
+                infors += infor.author + '\t评论了\t' + infor.title + '\n';
+
+            });
+            console.log(infors);
+
+            if (page < 20) {
+                getComments(url, (page + 20));
+            }
+            else {
+                console.log("爬取成功！");
+            }
         });
-        // console.log(comment);
-        comment.map(function (infor) {
-            infors += infor.author+'\t评论了\t'+infor.title+'\n';
-        });
-        console.log(infors);
-    });
+
+}
+
+function main() {
+    console.log('开始爬取页面');
+    getComments(url, page);
+}
+
+main();
+
+
+
